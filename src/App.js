f3e1ce339222d5aa1abb7cb13f4b47898794a8b8
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-10 10:48:18 +0000

    combined all the slide into one reusable component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 import * as React from 'react-dom';
 import { NavBar } from "./components/NavBar";
-import { WorkSlide } from "./components/Work";
-import { PlaySlide } from "./components/Play";
-/*import { StudySlide } from "./components/Study";*/
-import { ExerciseSlide } from "./components/Exercise";
-import { SocialSlide } from "./components/Social";
-import { SelfCareSlide } from "./components/SelfCare";
 import { Category } from './components/Category';
 
 function App() {
@@ -15,12 +9,12 @@ function App() {
           <main className="container box-border w-10/12 h-3/4 mx-auto grid 
           grid-cols-1 gap-4 text-white font-mono font-normal pt-8 md:pt-48 md:grid-cols-4">
               <div className="md:row-span-2"><NavBar/></div>
-              <WorkSlide/>
-              <PlaySlide/>
-              <Category/>
-              <ExerciseSlide/>
-              <SocialSlide/>
-              <SelfCareSlide/>
+              <Category type="Work" currentWeek="32hrs" lastWeek="Last Week - 36hrs" color="bg-yellow-500"/>
+              <Category type="Play" currentWeek="10hrs" lastWeek="Last Week - 8hrs" color="bg-blue-300"/>
+              <Category type="Study" currentWeek="4hrs" lastWeek="Last Week - 7hrs" color="bg-pink-400"/>
+              <Category type="Exercise" currentWeek="4hrs" lastWeek="Last Week - 5hrs" color="bg-green-200"/>
+              <Category type="Social" currentWeek="5hrs" lastWeek="Last Week - 10hrs" color="bg-purple-600"/>
+              <Category type="Self Care" currentWeek="2hrs" lastWeek="Last Week - 2hrs" color="bg-yellow-300"/>
           </main>
       </div>
   );
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,15 @@
-export const Category = () => {
+const DefaultClassName = "container flex flex-col-reverse border-0 border-solid rounded-lg h-36 md:h-full";
+
+export const Category = ({type, currentWeek, lastWeek, color}) => {
+    const mergeClassList = color + " " + DefaultClassName
     return(
-        <div className="container flex flex-col-reverse bg-pink-400 border-0 border-solid rounded-lg h-36 md:h-full">
-            <div className="container items-center flex flex-row box-border bg-indigo-900 border-0 border-solid
-                rounded-lg h-3/4">
+        <div className={mergeClassList}>
+            <div className="container items-center flex flex-row box-border border-0 border-solid rounded-lg h-3/4 bg-indigo-900">
                 <div className="container grid grid-cols-2 grid-rows-2 md:grid-rows-3">
-                    <h3 className="text-xs pl-4 pt-4">Study</h3>
+                    <h3 className="text-xs pl-4 pt-4">{type}</h3>
                     <div className="py-2 pr-4 text-right">...</div>
-                    <div className="pl-4 py-2 text-xl md:text-3xl md:py-0 md:col-span-2">4hrs</div>
-                    <div className="w-60 pr-4 text-right text-xs pt-4 md:pt-0 md:text-left md:pl-4">Last Week - 7hrs</div>
+                    <div className="pl-4 py-2 text-xl md:text-3xl md:py-0 md:col-span-2">{currentWeek}</div>
+                    <div className="w-60 pr-4 text-right text-xs pt-4 md:pt-0 md:text-left md:pl-4">{lastWeek}</div>
                 </div>
             </div>
         </div>
