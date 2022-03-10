10b94da71cf923d55c4e319c5430b3f4a39de988
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-10 09:20:49 +0000

    more styling to get slides to fit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
       <div className="bg-black h-screen">
           <main className="container box-border w-10/12 h-3/4 mx-auto grid grid-cols-1 
-          gap-4 gap-y-4 text-white font-mono font-normal pt-8 md:grid-cols-4">
+          gap-4 md:gap-y-0 text-white font-mono font-normal pt-8 md:grid-cols-4">
               <div className="md:row-span-2"><NavBar/></div>
               <WorkSlide/>
               <PlaySlide/>
