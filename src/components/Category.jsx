66e800e01f17f381662223d53ee36b9a28fbc45b
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-10 13:36:29 +0000

    improved padding and margins

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -4,12 +4,12 @@ export const Category = ({type, currentWeek, lastWeek, color}) => {
     const mergeClassList = color + " " + DefaultClassName
     return(
         <div className={mergeClassList}>
-            <div className="container items-center flex flex-row box-border border-0 border-solid rounded-lg h-3/4 bg-indigo-900">
+            <div className="container font-sans items-center flex flex-row box-border border-0 border-solid rounded-lg h-4/5 bg-indigo-900">
                 <div className="container grid grid-cols-2 grid-rows-2 md:grid-rows-3">
-                    <h3 className="text-xs pl-4 pt-4">{type}</h3>
-                    <div className="py-2 pr-4 text-right">...</div>
-                    <div className="pl-4 py-2 text-xl md:text-3xl md:py-0 md:col-span-2">{currentWeek}</div>
-                    <div className="w-60 pr-4 text-right text-xs pt-4 md:pt-0 md:text-left md:pl-4">{lastWeek}</div>
+                    <h3 className="text-sm md:text-xs pl-4 pt-0 md:pt-6 md:col-span-2">{type}</h3>
+                    <div className="md:pt-4 pt-0 pr-4 text-sm text-right">...</div>
+                    <div className=" pt-0 pl-4 text-3xl md:text-4xl md:pt-1 md:col-span-3">{currentWeek}</div>
+                    <div className="pt-2 pr-4 text-xs text-right md:w-60 md:pt-0 md:text-left md:pl-4">{lastWeek}</div>
                 </div>
             </div>
         </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,22 @@ import logo from "../images/image-jeremy.png";
 export const NavBar = () => {
     return(
         <nav className="container box-border bg-indigo-900 box-border border-0 border-solid rounded-lg h-36 md:h-full">
-            <div className="container bg-indigo-500 border-0 border-solid rounded-lg grid grid-cols-2 grid-rows-1 flex-row py-4 h-2/3
+            <div className="container bg-indigo-500 border-0 border-solid rounded-lg grid grid-cols-3 grid-rows-1 flex-row py-4 h-2/3
                   items-center justify-center md:grid-cols-1 md:grid-rows-2">
                 <img src={logo} alt="JR" className="rounded-full h-14 w-14 border-solid
-                      border-2 border-white mx-4"/>
-                <div className="flex flex-col text-left pl-4">
-                    <p className="text-sm text-gray-300">Report for</p>
-                    <div className="flex flex-row gap-x-1 items-left text-xl md:flex-col">
+                      border-2 border-white ml-8 md:ml-4"/>
+                <div className="flex flex-col text-left md:pl-4 col-span-2">
+                    <p className="text-xs text-gray-300">Report for</p>
+                    <div className="flex flex-row gap-x-1 items-left text-xl md:text-2xl md:flex-col">
                         <p>Jeremy</p>
                         <p>Robson</p>
                     </div>
                 </div>
             </div>
-            <div className="container grid grid-rows-1 grid-cols-3 justify-between items-center md:grid-cols-1 md:grid-rows-3">
-                <h3 className="py-1 pl-5 text-indigo-400 hover:bg-indigo-500">Daily</h3>
-                <h3 className="py-1 pl-5 hover:bg-indigo-500">Weekly</h3>
-                <h3 className="py-1 pl-5 pr-5 text-indigo-400 hover:bg-indigo-500">Monthly</h3>
+            <div className="container text-xs grid grid-rows-1 grid-cols-3 justify-between items-center md:grid-cols-1 md:grid-rows-3 pt-4">
+                <h3 className="md:py-1 pl-5 text-indigo-400 hover:bg-indigo-500">Daily</h3>
+                <h3 className="md:py-1 pl-5 hover:bg-indigo-500">Weekly</h3>
+                <h3 className="md:py-1 pl-5 pr-5 text-indigo-400 hover:bg-indigo-500">Monthly</h3>
             </div>
         </nav>
     );
