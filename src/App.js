410df2a2d3461bfac8b1922fbc3590292a587e13
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-03-10 09:59:26 +0000

    iniital set up of creating one component for all slides

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import './App.css';
 import * as React from 'react-dom';
-import {NavBar} from "./components/NavBar";
-import {WorkSlide} from "./components/Work";
-import {PlaySlide} from "./components/Play";
-import {StudySlide} from "./components/Study";
-import {ExerciseSlide} from "./components/Exercise";
-import {SocialSlide} from "./components/Social";
-import {SelfCareSlide} from "./components/SelfCare";
+import { NavBar } from "./components/NavBar";
+import { WorkSlide } from "./components/Work";
+import { PlaySlide } from "./components/Play";
+/*import { StudySlide } from "./components/Study";*/
+import { ExerciseSlide } from "./components/Exercise";
+import { SocialSlide } from "./components/Social";
+import { SelfCareSlide } from "./components/SelfCare";
+import { Category } from './components/Category';
 
 function App() {
   return (
-      <div className="bg-black h-screen">
-          <main className="container box-border w-10/12 h-3/4 mx-auto grid grid-cols-1 
-          gap-4 md:gap-y-0 text-white font-mono font-normal pt-8 md:grid-cols-4">
+      <div className="bg-black md:h-screen">
+          <main className="container box-border w-10/12 h-3/4 mx-auto grid 
+          grid-cols-1 gap-4 text-white font-mono font-normal pt-8 md:pt-48 md:grid-cols-4">
               <div className="md:row-span-2"><NavBar/></div>
               <WorkSlide/>
               <PlaySlide/>
-              <StudySlide/>
+              <Category/>
               <ExerciseSlide/>
               <SocialSlide/>
               <SelfCareSlide/>
