96cd74d0fd75ed1078ab12847eb8b4dee2f62b8b
Kerry Clements <kerryclements@me.com>
2026-06-24 11:13:25 +0100

    feat: add data, types and wire up timeframe switching

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,17 +1,38 @@
-const DefaultClassName = "container flex flex-col-reverse border-0 border-solid rounded-lg h-36 md:h-full";
+import { Activity, TimeFrame } from "../types";
+import { CATEGORY_COLORS, PREVIOUS_LABEL } from "../constants";
 
-export const Category = ({type, currentWeek, lastWeek, color}) => {
-    const mergeClassList = color + " " + DefaultClassName
-    return(
-        <div className={mergeClassList}>
-            <div className="container font-sans items-center flex flex-row box-border border-0 border-solid rounded-lg h-4/5 bg-indigo-900">
-                <div className="container grid grid-cols-2 grid-rows-2 md:grid-rows-3">
-                    <h3 className="text-sm md:text-xs pl-4 pt-0 md:pt-6 md:col-span-2">{type}</h3>
-                    <div className="md:pt-4 pt-0 pr-4 text-sm text-right cursor-pointer">...</div>
-                    <div className=" pt-0 pl-4 text-3xl md:text-4xl md:pt-1 md:col-span-3">{currentWeek}</div>
-                    <div className="pt-2 pr-4 text-xs text-right md:w-60 md:pt-0 md:text-left md:pl-4">{lastWeek}</div>
-                </div>
-            </div>
-        </div>
-    );
+const defaultClassName =
+  "container flex flex-col-reverse border-0 border-solid rounded-lg h-36 md:h-full";
+
+interface CategoryProps {
+  activity: Activity;
+  timeframe: TimeFrame;
 }
+
+export const Category = ({ activity, timeframe }: CategoryProps) => {
+  const { title, timeframes } = activity;
+  const { current, previous } = timeframes[timeframe];
+  const color = CATEGORY_COLORS[title];
+  const previousLabel = PREVIOUS_LABEL[timeframe];
+
+  return (
+    <div className={`${color} ${defaultClassName}`}>
+      <div className='container font-sans items-center flex flex-row box-border border-0 border-solid rounded-lg h-4/5 bg-indigo-900'>
+        <div className='container grid grid-cols-2 grid-rows-2 md:grid-rows-3'>
+          <h3 className='text-sm md:text-xs pl-4 pt-0 md:pt-6 md:col-span-2'>
+            {title}
+          </h3>
+          <div className='md:pt-4 pt-0 pr-4 text-sm text-right cursor-pointer'>
+            ...
+          </div>
+          <div className='pt-0 pl-4 text-3xl md:text-4xl md:pt-1 md:col-span-3'>
+            {current}hrs
+          </div>
+          <div className='pt-2 pr-4 text-xs text-right md:w-60 md:pt-0 md:text-left md:pl-4'>
+            {previousLabel} - {previous}hrs
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,45 @@
 import logo from "../images/image-jeremy.png";
+import { TimeFrame } from "../types";
 
-
-export const NavBar = () => {
-    return(
-        <nav className="container box-border bg-indigo-900 box-border border-0 border-solid rounded-lg h-36 md:h-full">
-            <div className="container bg-indigo-500 border-0 border-solid rounded-lg grid grid-cols-3 grid-rows-1 flex-row py-4 h-2/3
-                  items-center justify-center md:grid-cols-1 md:grid-rows-2">
-                <img src={logo} alt="JR" className="rounded-full h-14 w-14 border-solid
-                      border-2 border-white ml-8 md:ml-4"/>
-                <div className="flex flex-col text-left md:pl-4 col-span-2">
-                    <p className="text-xs text-gray-300">Report for</p>
-                    <div className="flex flex-row gap-x-1 items-left text-xl md:text-2xl md:flex-col">
-                        <p>Jeremy</p>
-                        <p>Robson</p>
-                    </div>
-                </div>
-            </div>
-            <div className="container text-xs grid grid-rows-1 grid-cols-3 md:grid-cols-1 md:grid-rows-3 pt-4">
-                <h3 className="text-centre md:py-1 pl-5 mr-5 text-indigo-400 cursor-pointer hover:bg-indigo-500">Daily</h3>
-                <h3 className="text-centre md:py-1 pl-5 pr-5 cursor-pointer hover:bg-indigo-500">Weekly</h3>
-                <h3 className="text-centre md:py-1 pl-5 cursor-pointer pr-5 text-indigo-400 hover:bg-indigo-500">Monthly</h3>
-            </div>
-        </nav>
-    );
+interface NavBarProps {
+  timeframe: TimeFrame;
+  onTimeFrameChange: (timeframe: TimeFrame) => void;
 }
+
+const timeframes: TimeFrame[] = ["daily", "weekly", "monthly"];
+
+export const NavBar = ({ timeframe, onTimeFrameChange }: NavBarProps) => {
+  return (
+    <nav className='container box-border bg-indigo-900 border-0 border-solid rounded-lg h-36 md:h-full'>
+      <div
+        className='container bg-indigo-500 border-0 border-solid rounded-lg grid grid-cols-3 grid-rows-1 py-4 h-2/3
+            items-center justify-center md:grid-cols-1 md:grid-rows-2'
+      >
+        <img
+          src={logo}
+          alt='Jeremy Robson'
+          className='rounded-full h-14 w-14 border-solid border-2 border-white ml-8 md:ml-4'
+        />
+        <div className='flex flex-col text-left md:pl-4 col-span-2'>
+          <p className='text-xs text-gray-300'>Report for</p>
+          <div className='flex flex-row gap-x-1 text-xl md:text-2xl md:flex-col'>
+            <p>Jeremy</p>
+            <p>Robson</p>
+          </div>
+        </div>
+      </div>
+      <div className='container text-xs grid grid-rows-1 grid-cols-3 md:grid-cols-1 md:grid-rows-3 pt-4'>
+        {timeframes.map((tf) => (
+          <button
+            key={tf}
+            onClick={() => onTimeFrameChange(tf)}
+            className={`capitalize md:py-1 pl-5 pr-5 text-left cursor-pointer hover:text-white
+              ${timeframe === tf ? "text-white" : "text-indigo-400"}`}
+          >
+            {tf}
+          </button>
+        ))}
+      </div>
+    </nav>
+  );
+};
